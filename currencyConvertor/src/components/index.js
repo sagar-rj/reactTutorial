ef3ed2b66e227e37ceabc5873bb7535e39d8ba24
Sagar <sgrraj1845@gmail.com>
2025-01-30 00:45:32 +0530

    currency convertor is done but some bugs need to be fixed

---
+++ currencyConvertor/src/components/index.js
@@ -0,0 +1,9 @@
+import InputBox from "./InputBox";
+
+export { InputBox };
+
+
+//In this code snippet, we are exporting the InputBox component from the components folder.
+//The InputBox component is imported and exported in the index.js file.
+// This allows us to import the InputBox component from the components folder using a single import statement in other files.
+// This is a common practice in React projects to organize and manage components in a modular way.
